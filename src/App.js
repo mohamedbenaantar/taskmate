689826bf253ea77e7a417e12c05b45cae7eb71c4
mohamedbenaantar <mohamedbenantar@gmail.com>
2023-09-16 13:02:23 +0100

    set Information into local storage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Header } from './components/Header';
 import { AddTask } from './components/AddTask';
 import { ShowTask } from './components/ShowTask';
 function App() {
 
-  const [taskList, setTaskList] = useState([] || JSON.parse(localStorage.getItem('taskList')));
+  const [taskList, setTaskList] = useState( JSON.parse(localStorage.getItem('taskList')) || []);
   // perfom a specific task for a specific element
   const [task, setTask] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem("taskList", JSON.stringify(taskList))
+  }, [taskList]);
   return (
     <div >
       <Header/>
