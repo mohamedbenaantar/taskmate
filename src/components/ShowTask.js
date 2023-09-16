b69c4f9fea459c2639dd5be54f432434a3a6b996
mohamedbenaantar <mohamedbenantar@gmail.com>
2023-09-16 10:11:19 +0100

    iterate some tasks

--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 export const ShowTask = () => {
+    const tasks = [
+    {id:10100, name:"Task A", time:"1:02:01 AM 9/13/2023"},
+    {id:10102, name:"Task B", time:"1:07:01 AM 9/13/2023"},
+    {id:10104, name:"Task C", time:"1:09:01 AM 9/13/2023"}
+]
   return (
     <section className="showTask">
         <div className="head">
@@ -13,14 +18,18 @@ export const ShowTask = () => {
         </div>
 
         <ul>
+            {tasks.map((task) => (
             <li>
-                <p>
-                    <span className="name">Task A</span>
-                    <span className="time">2020-01-01 12:31:10 am</span>
-                </p>
-                <i className="bi bi-pencil-fill"></i>
-                <i className="bi bi-trash"></i>
-            </li>
+            <p>
+                <span className="name">{task.name}</span>
+                <span className="time">{task.time}</span>
+            </p>
+            <i className="bi bi-pencil-fill"></i>
+            <i className="bi bi-trash"></i>
+        </li>
+            
+            ) )}
+            
         </ul>
     </section>
   )
